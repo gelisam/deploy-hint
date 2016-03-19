6c46fe99cc40aaad973714c29a15799eb099fa9e
Samuel Gélineau <gelisam+github@gmail.com>
2016-03-19 14:32:04 -0400

    use hint, and use the Prelude

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,28 @@
+import Text.Printf
+import Language.Haskell.Interpreter
+
+
+interpretDiag :: () -> Interpreter ((),())
+interpretDiag u = do
+    f <- interpret "\\x -> (x,x)" (as :: () -> ((),()))
+    return (f u)
+
+interpretId :: () -> Interpreter ()
+interpretId u = do
+    setImports ["Prelude"]
+    f <- interpret "id" (as :: () -> ())
+    return (f u)
+
 main :: IO ()
-main = putStrLn "typechecks."
+main = do
+    putStrLn "please type '()':"
+    u <- readLn
+    
+    r <- runInterpreter (interpretDiag u)
+    printf "(\\x -> (x,x)) %s is:\n" (show u)
+    print r
+    
+    putStrLn "and now, let's try the Prelude..."
+    r <- runInterpreter (interpretId u)
+    printf "id %s is:\n" (show u)
+    print r
